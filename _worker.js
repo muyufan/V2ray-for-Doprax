16532af8bd42bc8ac03346bce6c1c1711219c814
Dengke mu <92053+muyufan@users.noreply.github.com>
2023-03-14 16:37:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'testnvgp.eu-iiqsdfthygfv.dopraxrocks.net/'
+      url.hostname = 'testnvgp.eu-iiqsdfthygfv.dopraxrocks.net/vmess'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
